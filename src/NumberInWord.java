bcf67859ea6a24311d00da688198b6e6a30879b2
Jon Mockbee <mockbee1000@gmail.com>
2019-07-22 20:36:53 -0400

    initial commit

---
+++ src/NumberInWord.java
@@ -0,0 +1,28 @@
+public class NumberInWord {
+
+
+    public static void printNumberInWord (int num){
+
+        switch (num){
+
+            case 0:
+                System.out.println("zero");
+                break;
+
+            case 1:
+                System.out.println("one");
+                break;
+
+            case 2:
+                System.out.println("two");
+                break;
+
+        }
+    }
+
+
+
+
+
+
+}
